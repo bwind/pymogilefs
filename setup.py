6462d958b777a94d15bfaf3e5187f007e0cbea00
Bas Wind <mailtobwind@gmail.com>
2016-12-20 17:28:15 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pymogilefs',
-    version='0.1.0',
+    version='0.1.1',
     description='Python MogileFS Client',
     long_description='Python MogileFS Client',
     url='https://github.com/bwind/pymogilefs',
